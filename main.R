9648697c3b12783876d4d81ff903995d34921468
caillebotte Antoine <48158127+caillebotteAntoine@users.noreply.github.com>
2022-02-05 10:47:12 +0100

    first plot

--- main.R
+++ main.R
@@ -1,7 +1,4 @@
 
-
-
-
 pck <- c('tidyverse', 'ggplot2', 'reshape2')
 install.packages(setdiff(pck, rownames(installed.packages())))
 
@@ -10,4 +7,19 @@ require(tidyverse)
 require(ggplot2)
 require(reshape2)
 
-coucou
+
+data <- read.csv2('table_proteome_FH_all.csv') %>% 
+  select(-rep) %>% #rep représente le numero de réparition de l'exp ... pas utile
+  mutate_if(is.character, as.factor)
+
+str(data)
+View(data)
+
+not_var <- c('sample', 'group', 'temperature', 'imbibition')
+
+#juste pour le fun
+data[,1:20] %>% melt(id = not_var) %>% ggplot(aes(variable, value, fill = group)) +
+  geom_bar(position="dodge", stat="identity") +
+  theme(legend.position = "none")
+
+
